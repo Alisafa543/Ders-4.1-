081d6840b9a6d8f1cff75951d15dafb52b41e410
Əlisəfa Pirhesenov <elisefapirhesenov@gmail.com>
2024-11-29 12:47:15 +0400

    Tasklar 4.1

---
+++ Task (1).py
@@ -0,0 +1,4 @@
+my_dict = {"Data1" : 100 , "Data2" : -54 , "Data3" : 247}
+deyer = my_dict.values()
+a = sum(deyer)
+print(a)
---
+++ Task (2).py
@@ -0,0 +1,6 @@
+my_tuple1 = (1,2,3)
+my_tuple2 = (4,5,6)
+my_list1 = list(my_tuple1)
+my_list2 = list(my_tuple2)
+list = my_list1 + my_list2
+print(list)
